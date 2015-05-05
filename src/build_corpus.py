ba8112b39077646bbc8a8d2763c1bd0802c040a3
Khazhoyan Arsen <khazhoyan.arsen@gmail.com>
2015-05-05 20:16:13 +0300

    v.0.6.2 some refactoring

--- src/build_corpus.py
+++ src/build_corpus.py
@@ -2,7 +2,7 @@
 
 from subprocess import call
 
-with open('build/topics', 'r') as topics:
+with open('/data/papeeria/build/topics', 'r') as topics:
     for line in topics:
         call(['python3.4', '/data/papeeria/src/wiki_crawler.py', "'%s'" % line, '-ru'])
 
