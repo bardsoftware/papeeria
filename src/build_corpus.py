c36ee63ad87cabcf91feb0966bb7cc30ff3f0c3e
Khazhoyan Arsen <khazhoyan.arsen@gmail.com>
2015-05-05 20:19:49 +0300

    v.0.6.2 some refactoring

--- src/build_corpus.py
+++ src/build_corpus.py
@@ -5,6 +5,6 @@ from subprocess import call
 with open('/data/papeeria/build/topics', 'r') as topics:
     for line in topics:
         call(['python3.4', '/data/papeeria/src/wiki_crawler.py', "'%s'" % line, '-ru'])
-        print('%s has been processed' % line)
+        print('%s has been processed' % line[:-1])
 
 call(['classifier', 'index', '-ru'])
