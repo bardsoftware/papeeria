9e6b68d3bdeb784883dc32500660890b0d4c7392
Dmitry Barashev <dbarashev@bardsoftware.com>
2015-05-19 17:22:22 +0300

    added GET method returning response by PDF url

--- src/server.py
+++ src/server.py
@@ -2,13 +2,25 @@
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from subprocess import check_output
-
+from urllib.parse import urlparse, parse_qs
+from urllib.request import urlopen
 
 OK_CODE = 200
 DEFAULT_PORT = 8080
 
 
 class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        req_url = urlparse(self.path)
+        if req_url.path == '/q/pdf':
+            query_components = parse_qs(req_url.query)
+            url = query_components["url"][0]
+            with urlopen(url) as in_file, open("tmp/tmp.pdf", "wb") as out_file:
+                out_file.write(in_file.read())
+            out = check_output(['java', '-jar', 'jar/NER.jar', 'search', 'tmp', '-pdf', '-ru'])
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.wfile.write(out)
+
     def do_POST(self):
         length = self.headers['content-length']
         data = self.rfile.read(int(length))
