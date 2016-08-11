a197b6e1f82925c14798e018e0060fd01779370f
teners <dmemory7@ya.ru>
2016-08-11 15:02:17 +0300

    Removed output file option, added corrections with spaces support, fixed hunspell options to interpret input as latex file

--- backend/spellchecker/huntojson.sh
+++ backend/spellchecker/huntojson.sh
@@ -5,24 +5,21 @@ show_help() {
 cat << EOF
 Usage: ${0##*/} [options...] [file]
 Launch hunspell on file, convert output to JSON format 
-and write to standard output if used without '-o'.
+and write to standard output.
 
     -h         display this help and exit
-    -o output  file where to write JSON
     -d dict    use custom dictionaries
 
 Example: ${0##*/} demo.tex                      # launch hunspell on 'demo.tex'
-         ${0##*/} -o json.txt summer-2016.tex   # write output to 'json.txt'
          ${0##*/} -d ru_RU ru_demo.tex          # use russian dictionary
 EOF
 }
 
-OUTFILE=
 DICT=
+OPTIND=1
 
 # command line arguments processing
-OPTIND=1
-while getopts ":hd:o:" opt; do
+while getopts ":hd:" opt; do
     case "$opt" in
         h)
             show_help
@@ -30,9 +27,6 @@ while getopts ":hd:o:" opt; do
             ;;
         d)
             DICT="-p $OPTARG"
-            ;;
-        o)
-            OUTFILE="$OPTARG"
             ;;
         ?)
             echo -e "Invalid option: -$OPTARG\n" >&2
@@ -43,7 +37,7 @@ while getopts ":hd:o:" opt; do
 done
 shift $((OPTIND-1))
 
-if [ "$#" != 1 ]
+if [ "$#" != "1" ]
 then
     echo -e "Missing input file\n" >&2
     show_help >&2
@@ -52,30 +46,25 @@ fi
 
 INFILE=$1
 
-JSON=`
-    cat $INFILE  | 
-    hunspell -at | 
-    grep "^&.*"  | 
-    sed "s/,//g" | 
+JSON=$(
+    cat $INFILE    | 
+    hunspell -a -t | 
+    grep "^&.*"    | 
     awk $DICT '
     BEGIN { print "{" }
     {
         print "\t\""$2"\": [" ;
 
-        for (i = 5; i <= NF; i++) 
-            if (i != NF) 
-                {print "\t\t\""$i"\","} 
-            else 
-                {print "\t\t\""$i"\""} ;
+        split($0, split_string, ": ");
+        options_number=split(split_string[2], options, ", ");
+
+        for (i = 1; i <= options_number; i++) 
+            {print "\t\t\""options[i]"\","} 
         
         print "\t]," }
     END { print "}" }
-    '` 
+    '
+)
 
-if [ -n "$OUTFILE" ]
-then
-    echo $JSON > $OUTFILE
-else
-    echo $JSON
-fi
+echo $JSON
 
