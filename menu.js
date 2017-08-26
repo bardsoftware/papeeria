e1024b63d86eec35030badadfed66b5a71c08bc4
p0stante <alexandra.toropova@gmail.com>
2017-08-27 00:04:59 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,38 +1,8 @@
 $(document).ready(function(){
-    //$('nav ul li a').click(function () {
-    //    $(this).siblings('ul').toggleClass('active');
-    //});
-    //$('.submenu li').click(function () {
-    //    $('li').removeClass('active');
-    //    $(this).parents('li').addClass('active');
-    //    $(this).addClass('active');
-
-    //});
-    //$("li:not(.submenu li)").click(function () {
-    //    $('li').removeClass('active');
-    //    //$(this).parent('li').addClass('active');
-    //    $(this).addClass('active');
-    //    $(this).next("li:has(.submenu)").addClass('active');
-    //   // e.preventDefault();
-    //    //$(this).siblings('li').removeClass('active');
-    //    //$(this).siblings('li').children('li').removeClass('active');
-    //});
     $('li a').click(function () {
         $('li').removeClass('active');
+        $('li a').removeClass('active');
         $(this).parents('li').addClass('active');
         $(this).addClass('active');
-
     });
-    //$('li nav li').click(function () {
-    //    $(this).siblings('li').removeClass('active');
-    //    //$(this).parent('li').addClass('active');
-    //    $(this).toggle();
-
-        //$(this).siblings('li').removeClass('active');
-        //$(this).siblings('li').children('li').removeClass('active');
-    //});
-    //$('nav ul li a').click(function () {
-    //    $(this).children('ul').toggle();
-    //});
-
 });
